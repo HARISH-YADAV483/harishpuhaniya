3c1fa9367ce4ee8d8eeaf3b416e68dfce1a30649
KARAN YADAV <karanyadav@KARANs-MacBook-Air.local>
2026-04-12 22:56:40 +0530

    your message

--- backend/src/app.js
+++ backend/src/app.js
@@ -2,19 +2,19 @@ const express = require("express");
 const cors = require("cors");
 const app = express();
 
-const contactRoutes = require("./routes/contactRoutes");
+const contactController = require("./controllers/contactController");
 
 app.use(cors());
 app.use(express.json());
 
-app.use("/api/contact", contactRoutes);
+app.post("/api/contact", contactController.createContact);
 
 app.get("/", (req, res) => {
-    res.send("Backend is running successfully! 🚀 Check /api/hello for API status.");
+    res.send("Backend is running successfully!  Check /api/hello for API status.");
 });
 
 app.get("/api/hello", (req, res) => {
-    res.json({ message: "Hello from Express 🚀" });
+    res.json({ message: "Hello from Express " });
 });
 
 
